165ae96b9bf7a752007738a18f93d30b1c071ed9
jacobYun97 <ydy95j@naver.com>
2023-03-13 16:46:41 +0900

    :sparkles: reducer 완료
    히히

--- GlobalTest/src/domain/1_reducer/components/pages/Q1.js
+++ GlobalTest/src/domain/1_reducer/components/pages/Q1.js
@@ -38,7 +38,7 @@ const ReducerQ1Page = () => {
     });
   };
   const onDeleteIngredient = (id) => {
-    dispatch({ type: "DELETE_BTN", payload: { id } }); // ㅎㅎ
+    dispatch({ type: "DELETE_BTN", payload: { id } }); // ㅎ
   };
   console.log(ingredients);
   return (
